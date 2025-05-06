de8c905b3f16cdca2b84211e0f65f14ca37678ef
--global <truonghaonhien2822004@gmail.com>
2025-05-06 11:18:36 +0700

    feat: add default special events carousel
    using default settings of swiper.js. Not tested for mobile supports, the
    current ratio for banner images is 11:6 (1100x600 preferred resolution).

---
+++ frontend/src/components/special-events-carousel.tsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import firstBanner from "../assets/images/special_event_carousel_banner.png";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+
+const carouselImages = [
+  firstBanner, // Replace with your own images or paths
+  "https://placehold.co/1100x600", // Replace with your own images or paths
+  "https://placehold.co/1100x600", // Replace with your own images or paths
+];
+
+const SpecialEventsCarousel = () => {
+  return (
+    <section className="my-8 px-4 ">
+      <h2 className="w-5/6 mx-auto text-2xl font-bold text-[#1A0B49] mb-4">
+        Special Events
+      </h2>
+      <div className="w-5/6 h-auto mx-auto overflow-hidden shadow-lg">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          navigation
+          pagination={{ clickable: false }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: true,
+          }}
+          loop
+          className="w-full"
+        >
+          {carouselImages.map((src, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={src}
+                alt={`Event ${index + 1}`}
+                className="w-full aspect-auto object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialEventsCarousel;

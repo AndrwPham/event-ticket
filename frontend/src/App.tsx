c32ad5e42c53f83ab3148121cd572f926fa0cbde
--global <truonghaonhien2822004@gmail.com>
2025-05-08 17:48:19 +0700

    feat: basic Upcoming events grid
    some minor format changes. Basic layout for the Upcoming Events grid,
    not implemented routing using react dom.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 import Navbar from "./components/navbar";
 import SpecialEventsCarousel from "./components/special-events-carousel";
+import UpcomingEvents from "./components/UpcomingEvents.tsx";
 
 function App() {
   return (
     <>
       <Navbar />
       <SpecialEventsCarousel />
+      <UpcomingEvents />
     </>
   );
 }

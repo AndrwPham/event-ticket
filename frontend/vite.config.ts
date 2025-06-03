5ac484ccf0b633c50f4fc9b099536e3fb9d32dbc
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-06-03 13:14:34 +0000

    style: prettier on frontend/

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // import path from 'path';
 
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 5173,       // Optional: Customize the dev server port
+        port: 5173, // Optional: Customize the dev server port
         // open: true        // Optional: Automatically open in browser
     },
     build: {
-        outDir: 'dist',   // Default output directory
-        sourcemap: true   // Optional: Helpful for debugging
-    }
+        outDir: "dist", // Default output directory
+        sourcemap: true, // Optional: Helpful for debugging
+    },
 });

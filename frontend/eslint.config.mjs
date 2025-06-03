28e9c21908b1c7b511903b61f6d5962c4076b9aa
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-06-03 13:14:15 +0000

    chore: removing stylistic check on eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,29 +4,30 @@ import reactPlugin from "eslint-plugin-react";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
+/** @type {import("eslint").Linter.Config[]} */
 const eslintConfig = [
-  js.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
+    js.configs.recommended,
+    ...tseslint.configs.strict,
 
-  {
-    plugins: {
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-      "jsx-a11y": jsxA11yPlugin,
+    {
+        plugins: {
+            react: reactPlugin,
+            "react-hooks": reactHooksPlugin,
+            "jsx-a11y": jsxA11yPlugin,
+        },
+        files: ["**/*.tsx", "**/*.jsx"],
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "jsx-a11y/anchor-is-valid": "warn",
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+        },
     },
-    files: ["**/*.tsx", "**/*.jsx"],
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "jsx-a11y/anchor-is-valid": "warn",
-    },
-  },
 ];
 
 export default eslintConfig;

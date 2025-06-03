c0007d845ec7e1c4627ce84f8a3a1a4bc92ba41b
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-06-03 14:19:55 +0000

    chore: pre-commit testing for eslint and prettier

---
+++ .husky/check-frontend.js
@@ -0,0 +1,36 @@
+import { execSync } from 'child_process';
+
+function hasStagedChangesInFolder(folder) {
+    try {
+        const output = execSync(`git diff --cached --name-only ${folder}`, { encoding: 'utf-8' });
+        return output.trim().length > 0;
+    } catch {
+        return false;
+    }
+}
+
+const targetDir = "frontend";
+
+if (hasStagedChangesInFolder(targetDir)) {
+    process.chdir(targetDir);
+
+    try {
+        execSync('npm run lint', { stdio: 'inherit' });
+    } catch {
+        console.error('Lint check failed. Run "npm run lint" in frontend folder, and fix failing issues before committing.');
+        process.exit(1);
+    }
+
+    try {
+        execSync('npm run format:test', { stdio: 'inherit' });
+    } catch {
+        console.error('Format check failed. Please run "npm run format" in frontend folder before committing.');
+        process.exit(1);
+    }
+}
+else {
+    console.log("No staged change in frontend folder. Skipping check.")
+    process.exit(0);
+}
+
+

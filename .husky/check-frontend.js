9831e8922128034addc1606185b0e642bdd63bf2
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2025-06-05 07:03:57 +0000

    chore: Scoping checks. On commit check only staged changes, on PR check everything.

--- .husky/check-frontend.js
+++ .husky/check-frontend.js
@@ -1,36 +1,17 @@
 import { execSync } from 'child_process';
 
-function hasStagedChangesInFolder(folder) {
-    try {
-        const output = execSync(`git diff --cached --name-only ${folder}`, { encoding: 'utf-8' });
-        return output.trim().length > 0;
-    } catch {
-        return false;
-    }
+process.chdir("frontend");
+
+try {
+    execSync('npm run lint:test', { stdio: 'inherit' });
+} catch {
+    process.exit(1);
 }
 
-const targetDir = "frontend";
-
-if (hasStagedChangesInFolder(targetDir)) {
-    process.chdir(targetDir);
-
-    try {
-        execSync('npm run lint:test', { stdio: 'inherit' });
-    } catch {
-        console.error('Lint check failed. Run "npm run lint" in frontend folder, and fix issues before committing.');
-        process.exit(1);
-    }
-
-    try {
-        execSync('npm run format:test', { stdio: 'inherit' });
-    } catch {
-        console.error('Format check failed. Please run "npm run format" in frontend folder before committing.');
-        process.exit(1);
-    }
-}
-else {
-    console.log("No staged change in frontend folder. Skipping check.")
-    process.exit(0);
+try {
+    execSync('npm run format:test', { stdio: 'inherit' });
+} catch {
+    process.exit(1);
 }
 
 
---
+++ frontend/.script/lint-changed.js
@@ -0,0 +1,28 @@
+import { execSync } from "child_process";
+import path from "path";
+
+const targetDir = "frontend";
+
+// list out all changed file in targetDir.
+const changedFiles = execSync(`git diff --cached --name-only ../${targetDir}`)
+    .toString()
+    .split("\n")
+    .map((filePath) => filePath.trim())
+    .filter((file) => file.startsWith("frontend/src/") && /\.(ts|tsx|js|jsx)$/.test(file))
+    .map((filePath) => path.relative(targetDir, filePath));
+
+if (changedFiles.length === 0) {
+    console.log(`No staged change found in ${targetDir}, exiting.`);
+    process.exit(0);
+}
+
+try {
+    execSync(`npx eslint ${changedFiles.join(" ")}`, { stdio: "inherit" });
+} catch {
+    console.error(
+        "Lint check failed. Run 'npm run lint' to attempt to automatically rectify the problems, then fix the remaining one before committing.",
+    );
+    process.exit(1);
+}
+
+console.log("Everything seems good!")

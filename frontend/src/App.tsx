5ad4fe19e76694a94bcc5aa08cb6e5f6bcccee12
phuclhp1922 <nguyensongthienphuc@gmail.com>
2025-06-10 11:34:32 +0700

    Ad-hoc implementation of App.tsx to test Log-in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navbar from './components/navbar';
+import SpecialEventsCarousel from './components/special-events-carousel';
+import LogIn from './components/log-in';
+
 function App() {
-    return <></>;
+  const [isLogInOpen, setIsLogInOpen] = useState(false);
+
+  return (
+    <Router>
+      <Navbar onLogInClick={() => setIsLogInOpen(true)} />
+      <Routes>
+        <Route path="/" element={<SpecialEventsCarousel />} />
+        {/* Keep route version too if needed */}
+        {/* <Route path="/signup" element={<SignUp onClose={() => setIsSignUpOpen(false)} />} /> */}
+      </Routes>
+
+      {isLogInOpen && <LogIn onClose={() => setIsLogInOpen(false)} />}
+    </Router>
+  );
 }
+
 export default App;

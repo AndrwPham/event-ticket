ec30edf4a7959f27cab9134a5faa9541de7f8bd2
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-17 11:58:33 +0700

    feat: wiring up services and modules to Order.

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -4,10 +4,15 @@ import { OrderController } from './order.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
+import { IssuedTicketModule } from 'src/issuedticket/issuedticket.module';
+import { ClaimedTicketModule } from 'src/claimedticket/claimedticket.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { HoldService } from './hold.service';
 
 @Module({
-  imports: [JwtModule.register({}), AuthModule, PrismaModule],
+  imports: [JwtModule.register({}), AuthModule, PrismaModule, IssuedTicketModule, ClaimedTicketModule, RedisModule],
   controllers: [OrderController],
-  providers: [OrderService],
+  providers: [OrderService, HoldService],
+  exports: [OrderService],
 })
 export class OrderModule {}

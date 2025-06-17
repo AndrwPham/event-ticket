fa8c15261a8cc8a95fa0592796825c67c9049d10
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-17 12:17:44 +0700

    feat: create order using OrderDto.

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,14 +1,66 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable, ConflictException, InternalServerErrorException, BadRequestException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { HoldService } from './hold.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { IssuedTicketService } from '../issuedticket/issuedticket.service';
+import { ClaimedTicketService } from '../claimedticket/claimedticket.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class OrderService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly holdService: HoldService,
+    private readonly issuedTicketService: IssuedTicketService,
+    private readonly claimedTicketService: ClaimedTicketService
+  ) { }
 
   async create(dto: CreateOrderDto) {
-    
+    const { userId, ticketItems, totalPrice, method } = dto;
+
+    if (!ticketItems || ticketItems.length === 0) {
+      throw new BadRequestException('No ticket items provided');
+    }
+
+    // put on hold, if conflict, return ConflictException
+    await this.holdService.holdTickets(ticketItems, userId);
+
+    try {
+      const order = await this.prisma.$transaction(async (tx) => {
+        const createdOrder = await tx.order.create({
+          data: {
+            totalPrice,
+            status: 'PENDING',
+            method,
+            attendee: { connect: { id: userId } },
+          },
+        });
+
+        const claimPromises = ticketItems.map(async (ticketId) => {
+          tx.claimedTicket.create({
+            data: {
+              attendee: { connect: { id: userId } },
+              ticket: { connect: { id: ticketId } },
+              order: { connect: { id: createdOrder.id } },
+            },
+          });
+        });
+
+        await Promise.all(claimPromises);
+
+        return createdOrder;
+      });
+
+    } catch (error) {
+      await this.holdService.releaseTickets(ticketItems);
+
+      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new ConflictException('One or more tickets are not available or already claimed');
+      }
+
+      throw new InternalServerErrorException('Failed to create order');
+    }
   }
 
   async findAll() {

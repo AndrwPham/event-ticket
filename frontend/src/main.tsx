7faa13de3216129b644243d1ed2cd6ed0a20fd53
Andrw <anpd2004@gmail.com>
2025-06-17 10:46:36 +0700

    Remove: License error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { registerLicense } from "@syncfusion/ej2-base";
+const syncfusionLicenseKey = import.meta.env.VITE_SYNCFUSION_LICENSE_KEY;
+console.log("My Syncfusion License Key is:", syncfusionLicenseKey);
+if (syncfusionLicenseKey) {
+    registerLicense(syncfusionLicenseKey);
+} else {
+    console.warn(
+        "Syncfusion license key not found. Please check your .env file.",
+    );
+}
 
 const rootElement = document.getElementById("root");
 
---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,8 @@
+interface ImportMetaEnv {
+    readonly VITE_SYNCFUSION_LICENSE_KEY: string;
+    // you can add other environment variables here
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}

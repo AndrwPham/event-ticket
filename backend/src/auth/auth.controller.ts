974923663b42f66cc9b1b81d6a2be1f4a6015199
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-18 18:52:03 +0700

    refactor: introduce new endpoint for confirm and switching roles.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,42 +1,60 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Headers, Body, Controller, Get, Post, UseGuards, Logger, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { UserWithRoles } from './types/request-with-user.interface';
+import { SwitchRoleDto } from './dto/switch-role.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('register')
   register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   }
 
+  @Get('confirm')
+  confirm(@Query('token') token: string) {
+    return this.authService.confirmEmail(token);
+  }
+
   @Post('login')
-  async login(@Body() dto: LoginDto) {
-    const { username, password } = dto;
-    return this.authService.login(username, password);
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('switch-role')
+  switchRole(
+    @GetUser() user: UserWithRoles,
+    @Body() dto: SwitchRoleDto,
+  ) {
+    return this.authService.switchRole(user.userId, user.roles, dto);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('logout')
-  logout(@Req() req: any) {
-    return this.authService.logout(req.user.role, req.user.userId);
+  logout(@GetUser() user: UserWithRoles) {
+    return this.authService.logout(user.userId);
   }
 
   @UseGuards(JwtAuthGuard)
-  @Get('user')
-  getCurrentUser(@GetUser() user: any) {
-    return this.authService.getCurrentUser(user.role, user.userId);
+  @Get('me')
+  me(@GetUser() user: UserWithRoles) {
+    return this.authService.getCurrentUser(user.userId);
   }
 
   @UseGuards(JwtRefreshGuard)
   @Post('refresh')
-  refresh(@Req() req: any) {
-    const token = req.headers.authorization.split(' ')[1];
-    return this.authService.refreshTokens(req.user.role, req.user.userId, token);
+  refresh(
+    @GetUser('userId') userId: string,
+    @Headers('authorization') authHeader: string,
+  ) {
+    const rawRefreshToken = authHeader.split(' ')[1];
+    return this.authService.refreshTokens(userId, rawRefreshToken);
   }
 }

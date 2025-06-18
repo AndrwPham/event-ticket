0b1733b400df8b270645e73cf122d4d9e77e6a60
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-18 23:03:54 +0700

    refactor: dtos for claimed ticket creation.

--- backend/src/claimedticket/dto/create-claimedticket.dto.ts
+++ backend/src/claimedticket/dto/create-claimedticket.dto.ts
@@ -1,5 +1,17 @@
+import { ClaimedTicketStatus } from '../claimedticket-status.enum';
+import { IsString, IsOptional, IsEnum } from 'class-validator';
+
 export class CreateClaimedTicketDto {
+  @IsString()
   attendeeId: string;
+
+  @IsString()
   ticketId: string;
+
+  @IsString()
   orderId: string;
+
+  @IsOptional()
+  @IsEnum(ClaimedTicketStatus)
+  status?: ClaimedTicketStatus;
 }

044580402530c4124e124b6b82f824b6d6c56e4c
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-20 10:28:59 +0700

    feat: wiring handlers and providers to be use in service.

--- backend/src/notification/notification.module.ts
+++ backend/src/notification/notification.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { NotificationService } from './notification.service';
+import { AuthConfirmationHandler } from './handlers/auth-confirmation.handler';
+import { WelcomeHandler } from './handlers/welcome.handler';
+import { OrderReceiptHandler } from './handlers/order-receipt.handler';
+import { TicketDeliveryHandler } from './handlers/ticket-delivery.handler';
+import { NewsletterHandler } from './handlers/newsletter.handler';
+import { EmailProvider } from './providers/email.provider';
+import { FirebaseProvider } from './providers/firebase.provider';
 
 @Module({
   imports: [EventEmitterModule.forRoot()],
-  providers: [NotificationService],
+  providers: [
+    NotificationService,
+    AuthConfirmationHandler,
+    WelcomeHandler,
+    OrderReceiptHandler,
+    TicketDeliveryHandler,
+    NewsletterHandler,
+    EmailProvider,
+    FirebaseProvider,
+  ],
   exports: [NotificationService],
 })
 export class NotificationModule {}

ab6f36a691072d46b90a6edf6cd69daa0698fb5c
phuclhp1922 <nguyensongthienphuc@gmail.com>
2025-06-20 12:10:42 +0700

    Add label for input field

--- frontend/src/components/log-in.tsx
+++ frontend/src/components/log-in.tsx
@@ -35,6 +35,9 @@ export default function LogIn({ onClose }: LogInProps) {
                 <button onClick={onClose} className="absolute top-2 right-3 text-xl">&times;</button>
                 <h2 className="text-3xl font-bold text-center text-[#1D0E3C]">Log In</h2>
                 <form onSubmit={handleSubmit} className="space-y-4 mt-4">
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Email
+                    </label>
                     <input
                         type="email"
                         placeholder="Input email address or phone number"
@@ -43,6 +46,10 @@ export default function LogIn({ onClose }: LogInProps) {
                         required
                         className="w-full border rounded px-4 py-2"
                     />
+
+                    <label className="block text-sm font-medium text-gray-700 mb-1">
+                        Password
+                    </label>
                     <input
                         type="password"
                         placeholder="Input password"

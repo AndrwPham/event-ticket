96c40c1cd666f3f828b120b6107986928173c65f
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-20 17:40:09 +0700

    feat: basic event notification handler.

--- backend/src/notification/handlers/event-notification.handler.ts
+++ backend/src/notification/handlers/event-notification.handler.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { EventNotificationEvent } from '../events/event-notification.event';
+import { EmailProvider } from '../providers/email.provider';
+import { renderTemplateFromFile } from './template.helper';
 
 @Injectable()
 export class EventNotificationHandler {
+  constructor(private readonly emailProvider: EmailProvider) {}
+
   async handle(event: EventNotificationEvent) {
-    // TODO: Send transactional event notification to recipients
+    const templateData = { ...event.metadata, content: event.content };
+    const subject = event.subject;
+    const html = renderTemplateFromFile('event-notification', templateData, 'html');
+    const text = renderTemplateFromFile('event-notification', templateData, 'txt');
+    for (const recipient of event.recipients) {
+      await this.emailProvider.sendEmail(recipient, subject, html, text);
+    }
   }
 }

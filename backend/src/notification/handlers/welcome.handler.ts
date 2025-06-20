a2afe8418d188a5865c61ae769957c09d18a235b
fuisl <135719107+fuisl@users.noreply.github.com>
2025-06-20 11:09:31 +0700

    feat: welcome event for confirmed users.

--- backend/src/notification/handlers/welcome.handler.ts
+++ backend/src/notification/handlers/welcome.handler.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { UserCreatedEvent } from '../events/user-created.event';
+import { UserConfirmedEvent } from '../events/user-confirmed.event';
+import { EmailProvider } from '../providers/email.provider';
+import { renderTemplateFromFile } from './template.helper';
 
 @Injectable()
 export class WelcomeHandler {
-  async handle(event: UserCreatedEvent) {
-    // TODO: Send welcome email/message
+  constructor(private readonly emailProvider: EmailProvider) {}
+
+  async handle(event: UserConfirmedEvent) {
+    if (!event.email) throw new Error('Missing email for welcome notification');
+    const templateData = { name: event.name };
+    const subject = 'Welcome to Event Ticket!';
+    const html = renderTemplateFromFile('welcome', templateData, 'html');
+    const text = renderTemplateFromFile('welcome', templateData, 'txt');
+    await this.emailProvider.sendEmail(event.email, subject, html, text);
   }
 }

e561e56721f942dfcba6f8a134e3609599d59521
--global <truonghaonhien2822004@gmail.com>
2025-06-25 10:30:43 +0700

    feat: step 3 UI + organized App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SpecialEventsCarousel from './components/SpecialEventsCarousel';
-import CreateEvent from './components/CreateEvent'; 
+import CreateEvent from './pages/CreateEvent'; 
+import Step2TimeAndTickets from './components/event-creation/Step2TimeAndTickets';
 
 function App() {
 
-  return (
-    <Router>
-      <Navbar  />
-      <Routes>
-        <Route path="/" element={<SpecialEventsCarousel />} />
-        <Route path="/create-event" element={<CreateEvent />} /> 
-        {/* Keep route version too if needed */}
-        {/* <Route path="/signup" element={<SignUp onClose={() => setIsSignUpOpen(false)} />} /> */}
-      </Routes>
+    return (
+        <Router>
+            <Navbar  />
+            <Routes>
+                <Route path="/create-event" element={<CreateEvent />} /> 
+            </Routes>
 
-    </Router>
-  );
+        </Router>
+    );
 }
 
 export default App;
